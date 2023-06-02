218a00ece9d80f4727aa5896b28558574073b962
Huỳnh Ngọc Hưng <90973416+ngochung130201@users.noreply.github.com>
2023-06-02 16:22:45 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,35 @@ function App() {
       <Project animation="fade-down" classNameTitle={classNameTitle} />
       <Contact animation="fade-down" classNameTitle={classNameTitle}/>
       <Footer/>
-  
+       <div>
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <script>
+        {`
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "107844085658031");
+        chatbox.setAttribute("attribution", "install_email");
+        chatbox.setAttribute("attribution_version", "biz_inbox");
+        `}
+      </script>
+      <script>
+        {`
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml: true,
+            version: 'v17.0'
+          });
+        };
+
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+        `}
+      </script>
     </div>
   );
 }
